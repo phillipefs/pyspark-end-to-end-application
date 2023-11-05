5d3630f8d8ecf63428e2204c6dec35a3b0fe987d
Phillipe <phillipefs@msn.com>
2023-11-05 17:17:00 -0300

    update variables

--- utils/get_all_variables.py
+++ utils/get_all_variables.py
@@ -13,5 +13,5 @@ infer_schema = os.environ['inferSchema']
 ## Set Other Variables
 app_name         = 'USA Prescriber Research Report'
 current_path     = os.getcwd()
-staging_dim_city = current_path + 'staging/dimension_city'
-staging_fact     = current_path + 'staging/fact'
+staging_dim_city = current_path + '/staging/dimension_city'
+staging_fact     = current_path + '/staging/fact'
