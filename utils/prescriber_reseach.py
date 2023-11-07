3c5d613e7bf48c65f60aa6b02cbeca98b4ca8887
Phillipe <phillipefs@msn.com>
2023-11-07 19:45:57 -0300

    create functions reports

--- utils/prescriber_reseach.py
+++ utils/prescriber_reseach.py
@@ -10,6 +10,7 @@ from pyspark.sql.window import Window
 #Import Module Variables
 sys.path.insert(0, abspath(join(dirname(__file__), '..')))
 import utils.get_all_variables as var_project
+from utils.udfs import column_split_cnt
 
 #Load the Logging Configuration File
 logging.config.fileConfig(fname= "configs/logging_to_file.conf")
@@ -186,14 +187,44 @@ class PrescriberResearch:
                 .drop("presc_lname", "presc_fname")\
                 .dropna(subset=["presc_id", "drug_name"])\
                 .withColumn("trx_cnt", coalesce("trx_cnt", avg("trx_cnt").over(spec)))
-            
-            df_fact_clean.show(truncate=False)
+        
 
         except Exception as exp:
             logger.error("Error in the method - data_clean(). Please check the Stack Trace. " + str(exp))
             raise
 
-        return df_city_clean, df_fact_clean   
+        return df_city_clean, df_fact_clean
+    
+    def city_report(self, df_city_sel, df_fact_sel):
+        try:
+            logger.info(f"Transform - city_report() is started...")
+            df_city_split = df_city_sel.withColumn('zip_counts',column_split_cnt(df_city_sel.zips))
+            df_fact_grp = df_fact_sel.groupBy(df_fact_sel.presc_state, df_fact_sel.presc_city).agg(countDistinct("presc_id").alias("presc_counts"), sum("trx_cnt").alias("trx_counts"))
+            df_city_join = df_city_split.join(df_fact_grp,(df_city_split.state_id == df_fact_grp.presc_state) & (df_city_split.city == df_fact_grp.presc_city),'inner')
+            df_city_final = df_city_join.select("city","state_name","county_name","population","zip_counts","trx_counts","presc_counts")
+        except Exception as exp:
+            logger.error("Error in the method - city_report(). Please check the Stack Trace. " + str(exp),exc_info=True)
+            raise
+        else:
+            logger.info("Transform - city_report() is completed...")
+        return df_city_final
+
+
+    def top_5_prescribers(self, df_fact_sel):
+        try:
+            logger.info("Transform - top_5_Prescribers() is started...")
+            spec = Window.partitionBy("presc_state").orderBy(col("trx_cnt").desc())
+            df_presc_final = df_fact_sel.select("presc_id","presc_fullname","presc_state","country_name","years_of_exp","trx_cnt","total_day_supply","total_drug_cost") \
+            .filter((df_fact_sel.years_of_exp >= 20) & (df_fact_sel.years_of_exp <= 50) ) \
+            .withColumn("dense_rank",dense_rank().over(spec)) \
+            .filter(col("dense_rank") <= 5) \
+            .select("presc_id","presc_fullname","presc_state","country_name","years_of_exp","trx_cnt","total_day_supply","total_drug_cost")
+        except Exception as exp:
+            logger.error("Error in the method - top_5_Prescribers(). Please check the Stack Trace. " + str(exp),exc_info=True)
+            raise
+        else:
+            logger.info("Transform - top_5_Prescribers() is completed...")
+        return df_presc_final
 
     def start_pipeline(self):
         try:
@@ -212,8 +243,10 @@ class PrescriberResearch:
             #Clean Dataframes
             df_city, df_fact = process.data_clean(df_city , df_fact)
 
+            #Reports
+            df_city_report = process.city_report(df_city, df_fact)
+            df_top5_prescribers = process.top_5_prescribers(df_fact)
 
-            logging.info("start_pipeline() is Completed.")
 
         except Exception as error:
             logging.error("Error ocorred in the main() method." + str(error), exc_info=True)
